f5830b66707b7833db62b7bfdfe38347eb0c8608
Veni Gonzales <venibonnie@gmail.com>
2022-02-20 20:05:43 -0800

    Update summary

---
+++ main.js
@@ -0,0 +1,30 @@
+/* Open when someone clicks on the span element */
+function openNav1() {
+  document.getElementById("myNav1").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav1() {
+  document.getElementById("myNav1").style.width = "0%";
+}
+
+/* Open when someone clicks on the span element */
+function openNav2() {
+  document.getElementById("myNav2").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav2() {
+  document.getElementById("myNav2").style.width = "0%";
+}
+
+/* Open when someone clicks on the span element */
+function openNav3() {
+  document.getElementById("myNav3").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav3() {
+  document.getElementById("myNav3").style.width = "0%";
+}
+
